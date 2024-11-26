171fee89a3531d01b2accb3ffd39c3513e49fe78
Martin <martin.nygard@live.no>
2024-11-26 12:25:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="text-center mt-5">
+      <h1>Welcome to Holidaze</h1>
+      <Button variant="primary">Test Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
